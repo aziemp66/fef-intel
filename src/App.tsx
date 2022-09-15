633a6aa6cd2ab0365ed7bdf075dc9f3f6eb3f054
Azie Melza Pratama <aziemp66@gmail.com>
2022-09-15 22:07:20 +0700

    Working With Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./Components/UI/Header";
+import Router from "./Router/Router";
 
 function App() {
 	return (
 		<div className="w-full h-full ">
 			<Header />
+			<Router />
 		</div>
 	);
 }
--- src/Components/UI/Header.tsx
+++ src/Components/UI/Header.tsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
-		<div className="flex justify-center items-center flex-col">
-			<p className="font-poppins text-xl">With Poppins</p>
-			<p className="text-xl">Without Poppins</p>
-		</div>
+		<nav className="flex justify-between items-center w-full">
+			<div className="flex justify-center items-center gap-2">
+				<img src="/logo/intel-logo.png" alt="fef" className="w-24 h-24" />
+				<div className="w-[1px] h-16 bg-slate-400"></div>
+				<h1 className="font-poppins font-medium text-3xl p-2">FEF INTEL</h1>
+			</div>
+			<ul className="flex">
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/about-us">About Us</Link>
+				</li>
+				<li>
+					<Link to={"/competitions"}>Competitions</Link>
+				</li>
+				<li>
+					<Link to="/faq">FAQ</Link>
+				</li>
+			</ul>
+		</nav>
 	);
 };
 
