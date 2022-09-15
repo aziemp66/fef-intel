c18f3e9c959c371de5e03f75629a3dc23eb1ab0e
Azie Melza Pratama <aziemp66@gmail.com>
2022-09-15 21:27:49 +0700

    Setting Up Router

--- src/Components/UI/Header.tsx
+++ src/Components/UI/Header.tsx
@@ -5,7 +5,6 @@ const Header = () => {
 		<div className="flex justify-center items-center flex-col">
 			<p className="font-poppins text-xl">With Poppins</p>
 			<p className="text-xl">Without Poppins</p>
-			<img src="/logo/fef-logo.png" alt="fef" />
 		</div>
 	);
 };
