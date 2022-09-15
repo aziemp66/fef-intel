b9bee987b43693121c2180f5fc6ca316cb97b3e1
Azie Melza Pratama <aziemp66@gmail.com>
2022-09-15 21:10:48 +0700

    Adding Logos and Structuring Files

--- src/Components/UI/Header.tsx
+++ src/Components/UI/Header.tsx
@@ -4,7 +4,8 @@ const Header = () => {
 	return (
 		<div className="flex justify-center items-center flex-col">
 			<p className="font-poppins text-xl">With Poppins</p>
-			<p className="text-xl">With Poppins</p>
+			<p className="text-xl">Without Poppins</p>
+			<img src="/logo/fef-logo.png" alt="fef" />
 		</div>
 	);
 };
