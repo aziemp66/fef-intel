4ae33776eb622d28f16f6e7b7a0d546d9dad0e67
aziemp66 <aziemp66@gmail.com>
2022-09-17 20:12:05 +0700

    Adding Swiper

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div>
+			<Swiper
+				slidesPerView={1}
+				spaceBetween={30}
+				loop={true}
+				autoplay={{
+					delay: 2500,
+					disableOnInteraction: false,
+				}}
+				grabCursor
+				className="my-10"
+			>
+				<SwiperSlide>
+					<img
+						className="w-[1000px] h-[500px] mx-auto"
+						src="https://source.unsplash.com/1000x500?event"
+						alt="random"
+					/>
+				</SwiperSlide>
+				<SwiperSlide>
+					<img
+						className="w-[1000px] h-[500px] mx-auto"
+						src="https://source.unsplash.com/1000x500?party"
+						alt="random"
+					/>
+				</SwiperSlide>
+				<SwiperSlide>
+					<img
+						className="w-[1000px] h-[500px] mx-auto"
+						src="https://source.unsplash.com/1000x500?competition"
+						alt="random"
+					/>
+				</SwiperSlide>
+				<SwiperSlide>
+					<img
+						className="w-[1000px] h-[500px] mx-auto"
+						src="https://source.unsplash.com/1000x500?concert"
+						alt="random"
+					/>
+				</SwiperSlide>
+				<SwiperSlide>
+					<img
+						className="w-[1000px] h-[500px] mx-auto"
+						src="https://source.unsplash.com/1000x500?meeting"
+						alt="random"
+					/>
+				</SwiperSlide>
+			</Swiper>
+		</div>
+	);
 };
 
 export default Home;
