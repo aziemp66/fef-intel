7e3d697a06e8ee985aae4786a5b20d44a2a87112
Azie Melza Pratama <aziemp66@gmail.com>
2022-09-22 21:42:34 +0700

    Adding hover Effect

--- src/Components/Card/CardContainer.tsx
+++ src/Components/Card/CardContainer.tsx
@@ -3,16 +3,32 @@ import React from "react";
 type CardContainerProps = {
 	competition: "Scrabble" | "Speech" | "Debate" | "Newscasting";
 };
+type CardColorType = string;
 
 const CardContainer = ({ competition }: CardContainerProps) => {
-	const cardColor =
-		(competition === "Scrabble" && "bg-light-yellow") ||
-		(competition === "Speech" && "bg-light-green") ||
-		(competition === "Debate" && "bg-light-blue") ||
-		(competition === "Newscasting" && "bg-light-red");
+	let cardColor: CardColorType;
+	switch (competition) {
+		case "Scrabble":
+			cardColor = "bg-light-yellow";
+			break;
+		case "Speech":
+			cardColor = "bg-light-blue";
+			break;
+		case "Debate":
+			cardColor = "bg-light-green";
+			break;
+		case "Newscasting":
+			cardColor = "bg-light-red";
+			break;
+		default:
+			cardColor = "bg-light-yellow";
+			break;
+	}
+
+	const cardBrightColor = cardColor.replace("light", "bright");
 	return (
 		<div
-			className={`flex flex-col items-center justify-between ${cardColor} rounded-xl p-12 gap-4`}
+			className={`flex flex-col cursor-pointer items-center justify-between ${cardColor} rounded-xl p-12 gap-4 hover:${cardBrightColor} transition-all duration-300`}
 		>
 			<div className="flex justify-center items-center h-3/4">
 				<img
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
 				"bright-red": "#E9685C",
 				"bright-blue": "#8BB5BD",
 				"bright-green": "#91A784",
-				"bright-yellow": "#FFDD75",
+				"bright-yellow": "#FFD658",
 			},
 			boxShadow: {
 				"3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
