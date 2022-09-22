0530eb6704ffe96f26e1f3f8171a5fe8ff86ce78
Azie Melza Pratama <aziemp66@gmail.com>
2022-09-23 06:54:00 +0700

    Working with Homepage Responsiveness

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -68,27 +68,31 @@ const Home = () => {
 					/>
 				</SwiperSlide>
 			</Swiper>
-			<div className="flex justify-center items-center w-4/5 mx-auto gap-16 bg-light-pink h-[600px] rounded-xl shadow-2xl px-32">
-				<div className="w-[2000px] h-[2000px] flex justify-center items-center">
-					<img className="rounded-xl" src="/logo/fef-logo.png" alt="fef-logo" />
-				</div>
+			<div className="flex flex-col lg:flex-row justify-center items-center w-[full] lg:w-4/5 mx-auto lg:gap-16 bg-light-pink h-full rounded-xl shadow-2xl px-4 lg:px-24 py-8 lg:py-12">
+				<img
+					className="w-[250px] h-[200px] lg:w-[500px] lg:h-[300px]"
+					src="/logo/fef-logo.png"
+					alt="fef-logo"
+				/>
 
 				<div className="flex flex-col gap-6">
-					<h2 className="font-bold text-4xl">FEF INTEL UNSRI 2022</h2>
-					<p className="text-xl font-medium">
+					<h2 className="font-bold text-sm lg:text-4xl">
+						FEF INTEL UNSRI 2022
+					</h2>
+					<p className="text-sm lg:text-xl font-medium">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ipsum
 						euismod nullam proin ullamcorper sit enim sodales egestas. Phasellus
 						eget semper elit, blandit risus lacus, elementum pretium, egestas. A
 						sit integer egestas mauris. Mauris habitant nunc auctor commodo.
 					</p>
-					<p className="text-xl font-medium">
+					<p className="text-sm lg:text-xl font-medium">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
 						minus doloremque quasi temporibus ullam quam deserunt voluptas sint
 						repellendus sit.
 					</p>
 					<div>
 						<Link
-							className="bg-light-blue text-white px-6 py-2 font-semibold text-xl rounded-lg transition hover:bg-bright-blue duration-500"
+							className="bg-light-blue text-white px-6 py-2 font-semibold lg:text-xl rounded-lg transition hover:bg-bright-blue duration-500"
 							to={"/about-us"}
 						>
 							Learn More
@@ -105,7 +109,7 @@ const Home = () => {
 				<DashLineTitle color="yellow" text="Sponsorship" />
 			</div>
 
-			<div className="mx-auto w-1/2 h-[250px] bg-white border my-52 shadow-3xl rounded-xl"></div>
+			<div className="mx-auto w-3/4 h-[400px] bg-white border my-52 shadow-3xl rounded-xl"></div>
 		</div>
 	);
 };
