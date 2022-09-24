3fe574dafac2422ff61af5782948264d1b476e93
Azie Melza Pratama <aziemp66@gmail.com>
2022-09-24 10:13:41 +0700

    Adding Whatsapp Button

--- src/Components/UI/Footer.tsx
+++ src/Components/UI/Footer.tsx
@@ -5,7 +5,20 @@ const Footer = () => {
 	const location = useLocation();
 	const locationIsHome = location.pathname === "/";
 
-	return <div></div>;
+	return (
+		<div className="w-full h-[200px] bg-light-blue relative">
+			<a
+				href="http://www.google.com"
+				className="bg-green-400 rounded-full absolute -top-10 right-10 h-[75px] w-[75px] cursor-pointer"
+			>
+				<img
+					className="absolute w-full h-full"
+					src="/items/whatsapp.svg"
+					alt="whatsapp-icon"
+				/>
+			</a>
+		</div>
+	);
 };
 
 export default Footer;
