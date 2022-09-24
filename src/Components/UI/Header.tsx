8a2e363fafdf7d8031f581f9323ea761c6128e28
Azie Melza Pratama <aziemp66@gmail.com>
2022-09-24 11:03:44 +0700

    Adding Responsive Header

--- src/Components/UI/Header.tsx
+++ src/Components/UI/Header.tsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
-		<nav className="flex justify-between items-center w-full px-12">
-			<div className="flex justify-around items-center gap-2">
+		<nav className="flex justify-between md:justify-between items-center w-full px-12">
+			<div className="flex justify-center md:justify-around items-center gap-2">
 				<img src="/logo/intel-logo.png" alt="fef" className="w-24 h-24" />
 				<div className="w-[1px] h-16 bg-slate-400"></div>
-				<h1 className="font-medium text-3xl p-2">FEF INTEL</h1>
+				<h1 className="font-medium text-xl md:text-3xl p-2">FEF INTEL</h1>
 			</div>
-			<ul className="flex gap-20 px-8 w-full justify-center items-center">
+			<ul className="hidden md:flex gap-20 px-8 w-full justify-center items-center">
 				<li className="font-bold link-hover-animation text-xl">
 					<Link to="/">Home</Link>
 				</li>
@@ -23,6 +23,11 @@ const Header = () => {
 					<Link to="/faq">FAQ</Link>
 				</li>
 			</ul>
+			<div className="flex flex-col h-5 w-8 gap-1 cursor-pointer">
+				<div className="h-1 w-full bg-black rounded"></div>
+				<div className="h-1 w-full bg-black rounded"></div>
+				<div className="h-1 w-full bg-black rounded"></div>
+			</div>
 		</nav>
 	);
 };
