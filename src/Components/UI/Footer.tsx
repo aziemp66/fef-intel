bd3e056a284dde4ec8636102430d0a6a4c671006
Azie Melza Pratama <aziemp66@gmail.com>
2022-09-24 10:49:20 +0700

    Adding Gradient

--- src/Components/UI/Footer.tsx
+++ src/Components/UI/Footer.tsx
@@ -6,17 +6,25 @@ const Footer = () => {
 	const locationIsHome = location.pathname === "/";
 
 	return (
-		<div className="w-full h-[200px] bg-light-blue relative">
-			<a
-				href="http://www.google.com"
-				className="bg-green-400 rounded-full absolute -top-10 right-10 h-[75px] w-[75px] cursor-pointer"
-			>
-				<img
-					className="absolute w-full h-full"
-					src="/items/whatsapp.svg"
-					alt="whatsapp-icon"
-				/>
-			</a>
+		<div
+			className={`w-full ${
+				!locationIsHome
+					? "h-[100px] bg-light-blue relative"
+					: "h-[300px] bg-gradient-to-t from-bright-blue via-light-blue to-white absolute"
+			} `}
+		>
+			<div className="relative">
+				<a
+					href="http://www.google.com"
+					className="bg-green-400 rounded-full absolute -top-10 right-10 h-[75px] w-[75px] cursor-pointer"
+				>
+					<img
+						className="absolute w-full h-full"
+						src="/items/whatsapp.svg"
+						alt="whatsapp-icon"
+					/>
+				</a>
+			</div>
 		</div>
 	);
 };
