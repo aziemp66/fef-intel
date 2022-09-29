2cfefeedea39679712c95ba2e5b15ff66a413e22
Azie Melza Pratama <aziemp66@gmail.com>
2022-09-29 20:18:49 +0700

    Completed About Us Page

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,7 +1,57 @@
 import React from "react";
 
 const About = () => {
-	return <div>About</div>;
+	return (
+		<div className="my-24 ">
+			<div className="mb-8">
+				<h2 className="text-6xl font-bold text-center">FEF INTEL UNSRI 2022</h2>
+			</div>
+			<div className="flex flex-col justify-center items-center mb-8">
+				<div className="md:w-4/5 w-[90%] bg-light-yellow md:h-24 h-12 rounded-t-xl shadow-inner-lg"></div>
+				<div className="md:w-4/5 w-[90%] md:h-[450px] shadow-inner-lg"></div>
+			</div>
+			<div className="md:w-4/5 w-[90%] mx-auto flex flex-col gap-4">
+				<p className="text-justify">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+					nisi voluptatum impedit quisquam corrupti dolorum saepe aliquam! Omnis
+					ullam a tempora obcaecati inventore tenetur perspiciatis pariatur
+					voluptatum porro nihil vitae consectetur non, sint eaque repudiandae
+					reiciendis qui. Consectetur laudantium perferendis totam alias dolor
+					obcaecati nesciunt temporibus esse fuga porro dolores velit officiis
+					nisi, voluptates aliquid iusto, autem est ratione quaerat! In expedita
+					laboriosam nihil porro similique velit dolore obcaecati accusamus odit
+					consectetur minus atque, dolorem officia perferendis magni quaerat
+					quidem a aperiam iste vero! Magnam incidunt tenetur quisquam quos
+					molestiae sapiente facere. Asperiores ea voluptatibus nam repellat
+					consequatur dolore eligendi non ratione doloribus, amet consectetur at
+					nostrum obcaecati distinctio veniam accusamus. Iste voluptas
+					voluptatem ratione accusamus facere, odio delectus nostrum beatae
+					quasi, illo debitis pariatur nobis labore incidunt quo cumque, non
+					modi aspernatur nemo. Odio vero ut itaque amet unde assumenda sit
+					voluptate nihil dolore perferendis suscipit commodi officia rerum iste
+					ad nesciunt, quod veritatis sint reprehenderit blanditiis aliquam.
+					Omnis libero cum repudiandae a deserunt magni vitae, pariatur
+					voluptate sapiente quasi doloribus esse minus facilis quae dignissimos
+					praesentium. Temporibus, molestias nisi? Repellendus deleniti dolores
+					debitis quos alias id dicta consequatur, provident reprehenderit
+					consequuntur eveniet voluptatum doloremque aut nemo. Sunt, incidunt!
+				</p>
+				<p className="text-justify">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas saepe
+					aperiam natus dolor architecto placeat consequatur blanditiis corrupti
+					sint ut expedita maiores error sed quae, eos reiciendis, officiis ipsa
+					omnis perspiciatis repudiandae accusantium molestias quibusdam
+					voluptas. Ipsa quo voluptatum sunt minima dolorum quae nisi facilis
+					est asperiores? Quisquam assumenda nulla quam debitis quae, id vitae
+					vero tempora. Maiores alias natus tempora quis quas. Provident
+					exercitationem incidunt, doloremque explicabo molestiae nostrum neque
+					ex. Quaerat, animi deserunt optio doloribus autem veritatis, nisi,
+					ratione facere rem ut illum cumque tempore natus corrupti vitae
+					provident eos magni. Iure nisi velit, minima rerum deleniti rem.
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default About;
