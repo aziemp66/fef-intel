5adc6b9fd0be0308f28a3c5eef00409af1cb34e4
FadhilAF <falfatih3421@gmail.com>
2022-10-03 18:27:41 +0700

    fix

--- src/Components/WhatsappCard/WhatsappCard.tsx
+++ src/Components/WhatsappCard/WhatsappCard.tsx
@@ -3,7 +3,7 @@ import WhatsappButton from "../UI/WhatsappButton/WhatsappButton";
 const WhatsappCard = (props: { name: string; number: string }) => {
 	return (
 		<div className="flex justify-start gap-4 rounded-xl shadow-md py-4 sm:px-8">
-			<WhatsappButton className="w-14 h-14" contact="6281271467247" />
+			<WhatsappButton className="w-14 h-14" contact={props.number} />
 			<div>
 				<p className="text-lg font-bold">{props.name}</p>
 				<p className="text-lg">{props.number}</p>
