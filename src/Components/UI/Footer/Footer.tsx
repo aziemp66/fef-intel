401bb7410f96b3ab66c1146d55c47cb845b23fbf
FadhilAF <falfatih3421@gmail.com>
2022-10-03 10:31:09 +0700

    finished suruhan

--- src/Components/UI/Footer/Footer.tsx
+++ src/Components/UI/Footer/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
 
 	return (
 		<div
-			className={`w-full flex flex-col items-center ${
+			className={`w-full flex flex-col items-center space-y-4 ${
 				!locationIsHome
 					? "h-[100px] bg-light-blue relative"
 					: "h-[300px] bg-gradient-to-t from-bright-blue via-light-blue to-white absolute"
@@ -20,18 +20,38 @@ const Footer = () => {
 				<img className="h-24" src="/logo/fef-logo.png" alt="fef intel" />
 			</div>
 			<div className="flex space-x-1">
-				<div className="h-12 w-12 rounded-full bg-white flex justify-center items-center">
+				<a
+					href="https://instagram.com/intelunsri"
+					className="h-12 w-12 rounded-full bg-white flex justify-center items-center shadow-lg"
+				>
 					<InstagramLogo />
-				</div>
-				<div className="h-12 w-12 rounded-full bg-white flex justify-center items-center">
+				</a>
+				<a
+					href="https://www.facebook.com/intelunsri"
+					className="h-12 w-12 rounded-full bg-white flex justify-center items-center shadow-lg"
+				>
 					<FacebookLogo />
-				</div>
-				<div className="h-12 w-12 rounded-full bg-white flex justify-center items-center">
+				</a>
+				<a
+					href="http://line.me/ti/p/@cwn7838e"
+					className="h-12 w-12 rounded-full bg-white flex justify-center items-center shadow-lg"
+				>
 					<LineLogo />
-				</div>
-				<div className="h-12 w-12 rounded-full bg-white flex justify-center items-center">
+				</a>
+				<a
+					// https://stackoverflow.com/a/68000520/13673444
+					href="https://mail.google.com/mail/?view=cm&fs=1&to=intelunsri@gmail.com&su=FEF INTEL&body=Hello, I'm {name} from {institution}. I wanted to ask something about the fef event."
+					className="h-12 w-12 rounded-full bg-white flex justify-center items-center shadow-lg"
+				>
 					<GmailLogo />
-				</div>
+				</a>
+			</div>
+			<div className="text-center">
+				<p>
+					Info | Contact Us | Support | Terms of Service | Privacy Policy |
+					Licenses
+				</p>
+				<p>@Copyright INTEL 2022</p>
 			</div>
 		</div>
 	);
