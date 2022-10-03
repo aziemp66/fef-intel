db8b1b9161e281da0bfdebb6cdc4c885ead80927
FadhilAF <falfatih3421@gmail.com>
2022-10-03 08:24:18 +0700

    update footer

--- src/Components/UI/Footer/Footer.tsx
+++ src/Components/UI/Footer/Footer.tsx
@@ -1,5 +1,8 @@
-import React from "react";
 import { useLocation } from "react-router-dom";
+import { ReactComponent as InstagramLogo } from "../../../Assets/Icons/instagram.svg";
+import { ReactComponent as FacebookLogo } from "../../../Assets/Icons/facebook.svg";
+import { ReactComponent as LineLogo } from "../../../Assets/Icons/line.svg";
+import { ReactComponent as GmailLogo } from "../../../Assets/Icons/gmail.svg";
 
 const Footer = () => {
 	const location = useLocation();
@@ -7,12 +10,30 @@ const Footer = () => {
 
 	return (
 		<div
-			className={`w-full ${
+			className={`w-full flex flex-col items-center ${
 				!locationIsHome
 					? "h-[100px] bg-light-blue relative"
 					: "h-[300px] bg-gradient-to-t from-bright-blue via-light-blue to-white absolute"
 			} `}
-		></div>
+		>
+			<div className="items-center">
+				<img className="h-24" src="/logo/fef-logo.png" alt="fef intel" />
+			</div>
+			<div className="flex space-x-1">
+				<div className="h-12 w-12 rounded-full bg-white flex justify-center items-center">
+					<InstagramLogo />
+				</div>
+				<div className="h-12 w-12 rounded-full bg-white flex justify-center items-center">
+					<FacebookLogo />
+				</div>
+				<div className="h-12 w-12 rounded-full bg-white flex justify-center items-center">
+					<LineLogo />
+				</div>
+				<div className="h-12 w-12 rounded-full bg-white flex justify-center items-center">
+					<GmailLogo />
+				</div>
+			</div>
+		</div>
 	);
 };
 
