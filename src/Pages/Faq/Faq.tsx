ecd778bc0d96f9581e9642631e4ff7a9e1a67aa0
FadhilAF <falfatih3421@gmail.com>
2022-10-18 19:40:37 +0700

    add faq

--- src/Pages/Faq/Faq.tsx
+++ src/Pages/Faq/Faq.tsx
@@ -2,6 +2,52 @@ import DashLineTitle from "../../Components/UI/DashLineTitle/DashLineTitle";
 import FaqDropdown from "../../Components/FaqDropdown/FaqDropdown";
 
 const Faq = () => {
+	const faqs = [
+		[
+			"Is the competition open for Highschool students?",
+			"Yes! this competition is also available for Highschool students, so register yourself now and try your best🤩",
+		],
+		[
+			"Is it possible if one person participates in 2 competitions?",
+			"1 participant can join in 2 competitions if they’re capable",
+		],
+		[
+			"What’s the theme for each competition?",
+			"Since our main theme is about Health, so the specific same with",
+		],
+		[
+			"What’s the procedure for joining the competition?",
+			"Go to our google form link bit.ly/FasilkomEnglishFestival2022  and register. After that complete the payment",
+		],
+		[
+			"If I'm not from Indonesia but want to join, how about the payment?",
+			"No worries, you can use Paypal as well!",
+		],
+		[
+			"Is this competition open to everyone not only from Indonesia?",
+			"Absolutely😉",
+		],
+		[
+			"What’s parliament will be used for the debate competition?",
+			"The parliament will be using British",
+		],
+		[
+			"How’s the procedure for the Scrabble competition?",
+			"Scrabble competition will be held using Zoom and will use an application chosen by the host. ",
+		],
+		[
+			"When does the competition start?",
+			"The competition starts on 6 november until 15",
+		],
+		[
+			"Where can we submit the video?",
+			"Videos can be submitted on the registration website",
+		],
+		[
+			"How do I register to be a participant?",
+			"You can register at this link!!Come and join us🤩🥳 (https://bit.ly/FasilkomEnglishFestival2022)",
+		],
+	];
 	return (
 		<div className="text-center">
 			<DashLineTitle className="mb-4" color="yellow" text="FAQ" />
@@ -10,33 +56,17 @@ const Faq = () => {
 				answer!
 			</p>
 			<div className="items-center my-8 mx-4 space-y-16">
-				<FaqDropdown
-					color="#C8E3B8"
-					question="
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt voluptatibus sunt nemo necessitatibus enim suscipit eveniet, assumenda vel. Cupiditate, quia!
-          "
-					answer="
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto ipsa consequuntur quia quos odit sint provident officia facere ducimus ex commodi, quisquam asperiores corrupti a voluptatum atque doloremque voluptates modi esse assumenda aperiam quae non itaque saepe. Minus nisi sapiente, hic culpa, ipsam maxime iusto id fugit molestiae ratione architecto!
-          "
-				/>
-				<FaqDropdown
-					color="#FBEBBA"
-					question="
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit odit perspiciatis officia!
-          "
-					answer="
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit magnam perspiciatis doloremque culpa nemo expedita saepe pariatur libero, eius aut et eum accusantium error molestiae provident qui quas autem quod illum laboriosam in excepturi cum.
-          "
-				/>
-				<FaqDropdown
-					color="#C8E3B8"
-					question="
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, libero. Molestias, libero. Voluptatibus, iste similique?
-          "
-					answer="
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, unde, facere quasi doloribus alias eos consequatur explicabo ipsam et reiciendis quisquam!
-          "
-				/>
+				{faqs.map((faq, index) => {
+					if (index % 2 == 0) {
+						return (
+							<FaqDropdown color="#C8E3B8" question={faq[0]} answer={faq[1]} />
+						);
+					} else {
+						return (
+							<FaqDropdown color="#FBEBBA" question={faq[0]} answer={faq[1]} />
+						);
+					}
+				})}
 			</div>
 		</div>
 	);
